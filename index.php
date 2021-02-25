2860758febbeb25e9ae4132097dc11e2c28bd3ab
benz830 <77878804+benz830@users.noreply.github.com>
2021-02-25 19:38:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,19 +11,11 @@
 <br> the financial resources and essentials for a minimum standard of living. 
 <br> Poverty means that the income level from employment is so low that basic
 <br> human needs can't be met.</b></font></br>
-  <br>
 <br><b><h2><font color="yellow"> What is the Cycle of Poverty </b></h2><font>
 <font color="gray"><b> Poverty can be a trap. For someone to get out of poverty, they need opportunities such as education,
 <br> clean water, medical facilities nearby, and financial resources. Without these basic elements, poverty 
 <br>becomes a cycle from one generation to the next.<font></b>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<a href="https://scaffoldpoverty.herokuapp.com/?fbclid=IwAR1OndBXgBJQDQEqDP16G-xBHaifgEej223x_0jisnk3wF5ROqN3fSAGoX0"><center> HOME PAGE </a><p>
+<br><p align="center"><img width ="150px" img src="pvt.jpg">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img width ="150px" img src="povertyimg.jfif">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img width ="150px" img src="poverty3.jpg"></p>
+<br><a href="https://scaffoldpoverty.herokuapp.com/?fbclid=IwAR1OndBXgBJQDQEqDP16G-xBHaifgEej223x_0jisnk3wF5ROqN3fSAGoX0"><center> HOME PAGE </a><p>
 </body>
 </html>
